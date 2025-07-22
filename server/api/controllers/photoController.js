6187ea16b79c992947bb87b6250ecbba75ef394a
IbragimKhaniev <newinterny@gmail.com>
2025-07-22 21:40:56 +0300

    Update files

--- server/api/controllers/photoController.js
+++ server/api/controllers/photoController.js
@@ -1,5 +1,5 @@
-const Photo = require('../models/Photo');
-const User = require('../models/User');
+const Photo = require('../../models/Photo');
+const User = require('../../models/User');
 
 // Upload a photo
 exports.uploadPhoto = async (req, res) => {
